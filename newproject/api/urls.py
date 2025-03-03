dfeecad8c170d01f33dde931c8530e678fe3b741
johnson <ibitoyejohnson234@gmail.com>
2025-03-03 06:06:34 +0100

    Rest api

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_users, create_user
+from .views import get_users, create_user, user_detail
 
 urlpatterns = [
     path('user/', get_users, name='get_users'),
-    path('user/create/', create_user, name='create_user')
+    path('user/create/', create_user, name='create_user'),
+    path('user/<int:pk>', user_detail, name='user_detail')
 ]
